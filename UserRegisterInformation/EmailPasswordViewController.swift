14262a4bbba15547f984040b7be0055d91c76300
Pavlo <pavlo.olshansky@gmail.com>
2018-12-14 10:01:38 +0200

    add Email password view controller

---
+++ UserRegisterInformation/EmailPasswordViewController.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+class EmailPasswordViewController: UIViewController {
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView! {didSet{activityIndicator.stopAnimating()}}
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passTF: UITextField!
+    @IBOutlet weak var passConfirmFT: UITextField!
+
+    private let presenter = SignUpPresenter(signUp: UserDataManager())
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        presenter.attachView(view: self)
+        
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func secure(_ sender: Any) {
+        passTF.isSecureTextEntry = !passTF.isSecureTextEntry
+        passConfirmFT.isSecureTextEntry = !passConfirmFT.isSecureTextEntry
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    @IBAction func confirm(_ sender: Any) {
+        if let email = emailTF.text, let pass = passTF.text, passTF.text == passConfirmFT.text {
+                presenter.setEmailAndPass(email: email, pass: pass)
+        } else {
+            AlertDialog.showAlert("Ошибка", message: "Невалидный email, короткий пароль или пароли не совпадают", viewController: self)
+        }
+    }
+    
+}
+
+extension EmailPasswordViewController: SignUpViewProtocol {
+    func notUpdated() {}
+    func next() {}
+    func startLoading() {}
+    func finishLoading() {}
+    func setUser(user: UserVO) {}
+    func userCreated() {}
+    func userNotCreated() {}
+}
+
+
