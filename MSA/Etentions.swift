353b2503b2169e1adc18c1a0ad73a20ede16817b
Messer <mikle0497@gmail.com>
2019-01-08 00:10:30 +0200

    Add datePicker, delegate, etention

---
+++ MSA/Etentions.swift
@@ -0,0 +1,346 @@
+import Foundation
+import UIKit
+
+public let lightGREEN = UIColor(red: 22/255, green: 219/255, blue: 147/255, alpha: 1)
+public let lightRED = UIColor(red: 247/255, green: 23/255, blue: 53/255, alpha: 1)
+public let lightBLUE = UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 0.10)
+public let lightBlue_ = UIColor(red: 72/255, green: 157/255, blue: 255/255, alpha: 1.0)
+public let darkCyanGreen = UIColor(red: 25/255, green: 100/255, blue: 126/255, alpha: 1.0)
+public let darkCyanGreen45 = UIColor(red: 25/255, green: 100/255, blue: 126/255, alpha: 0.45)
+public let lightWhiteBlue = UIColor(red: 3/255, green: 175/255, blue: 255/255, alpha: 1.0)
+public let darkGreenColor = UIColor(red: 1/255, green: 76/255, blue: 102/255, alpha: 1.0)
+
+private var maxLengths = [UITextField: Int]()
+
+public func setShadow(outerView: UIView, shadowOpacity: Float) {
+    outerView.clipsToBounds = false
+    outerView.layer.shadowColor = UIColor.black.cgColor
+    outerView.layer.shadowOpacity = shadowOpacity
+    outerView.layer.shadowOffset = CGSize.zero
+    outerView.layer.shadowRadius = 10
+    outerView.layer.shadowPath = UIBezierPath(roundedRect: outerView.bounds, cornerRadius: 10).cgPath
+}
+
+public func delay(sec: Double, function: @escaping ()->()) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + sec, execute: {
+        function()
+    })
+}
+
+extension UITableView {
+    //    func reloadData(with animation: UITableViewRowAnimation) {
+    //        reloadSections(IndexSet(integersIn: 0..<numberOfSections), with: animation)
+    //    }
+}
+
+extension String {
+    
+    func getDate() -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        return dateFormatter.date(from: self)
+    }
+    
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).uppercased() + dropFirst()
+    }
+    
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+    
+}
+
+extension Array where Element: Equatable {
+    mutating func remove(_ item:Element ) {
+        for (index, element) in self.enumerated() {
+            if element == item {
+                self.remove(at: index)
+            }
+        }
+    }
+    
+    func unique<T:Hashable>(map: ((Element) -> (T)))  -> [Element] {
+        var set = Set<T>() //the unique list kept in a Set for fast retrieval
+        var arrayOrdered = [Element]() //keeping the unique list of elements but ordered
+        for value in self {
+            if !set.contains(map(value)) {
+                set.insert(map(value))
+                arrayOrdered.append(value)
+            }
+        }
+        
+        return arrayOrdered
+    }
+    
+    public func array( removing item:Element ) -> [Element] {
+        var result = self
+        result.remove( item )
+        return result
+    }
+}
+
+
+extension UINavigationItem {
+    func setTitle(title:String, subtitle:String) {
+        let titleLabel = UILabel(frame: CGRect(x: 0, y: -2, width: 0, height: 0))
+        
+        titleLabel.backgroundColor = UIColor.clear
+        titleLabel.textColor = UIColor.black
+        titleLabel.font = UIFont(name: "Rubik-Medium", size: 14)!
+        titleLabel.text = title
+        titleLabel.sizeToFit()
+        
+        let subtitleLabel = UILabel(frame: CGRect(x: 0, y: 18, width: 0, height: 0))
+        subtitleLabel.backgroundColor = UIColor.clear
+        subtitleLabel.textColor = UIColor.black
+        subtitleLabel.font = UIFont(name: "Rubik", size: 11)!
+        subtitleLabel.text = subtitle
+        subtitleLabel.sizeToFit()
+        
+        let titleView = UIView(frame: CGRect(x: 0, y: 0, width: max(titleLabel.frame.size.width, subtitleLabel.frame.size.width), height: 30))
+        titleView.addSubview(titleLabel)
+        titleView.addSubview(subtitleLabel)
+        
+        let widthDiff = subtitleLabel.frame.size.width - titleLabel.frame.size.width
+        
+        if widthDiff < 0 {
+            let newX = widthDiff / 2
+            subtitleLabel.frame.origin.x = abs(newX)
+        } else {
+            let newX = widthDiff / 2
+            titleLabel.frame.origin.x = newX
+        }
+        
+        self.titleView = titleView
+    }
+}
+extension UIView {
+    func setShadow(shadowOpacity: Float) {
+        self.clipsToBounds = false
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = shadowOpacity
+        self.layer.shadowOffset = CGSize.zero
+        self.layer.shadowRadius = 4
+        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: 12).cgPath
+    }
+    
+    func image() -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(bounds.size, isOpaque, 0)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return UIImage()
+        }
+        layer.render(in: context)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+    
+}
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
+
+extension String {
+    
+    var length: Int {
+        return self.characters.count
+    }
+    
+//    subscript (i: Int) -> String {
+//        return self[Range(i ..< i + 1)]
+//    }
+//    
+//    func substring(from: Int) -> String {
+//        return self[Range(min(from, length) ..< length)]
+//    }
+//    
+//    func substring(to: Int) -> String {
+//        return self[Range(0 ..< max(0, to))]
+//    }
+//    
+//    subscript (r: Range<Int>) -> String {
+//        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+//                                            upper: min(length, max(0, r.upperBound))))
+//        let start = index(startIndex, offsetBy: range.lowerBound)
+//        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+//        return String(self[Range(start ..< end)])
+//    }
+//    
+}
+
+extension UIImage {
+    func scaleImage(toSize newSize: CGSize) -> UIImage? {
+        let newRect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height).integral
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+        if let context = UIGraphicsGetCurrentContext() {
+            context.interpolationQuality = .high
+            let flipVertical = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: newSize.height)
+            context.concatenate(flipVertical)
+            context.draw(self.cgImage!, in: newRect)
+            let newImage = UIImage(cgImage: context.makeImage()!)
+            UIGraphicsEndImageContext()
+            return newImage
+        }
+        return nil
+    }
+    
+    func scaleAndRotateImage() -> UIImage {
+        let maxSize = self.size.width
+        guard let imgRef = self.cgImage
+            else { return self }
+        
+        let width = CGFloat(imgRef.width)
+        let height = CGFloat(imgRef.height)
+        
+        var transform: CGAffineTransform = .identity
+        
+        var bounds = CGRect(x: 0, y: 0, width: width, height: height)
+        
+        if width > maxSize || height < maxSize {
+            let ratio = width / height
+            
+            if ratio > 1 {
+                bounds.size.width = maxSize
+                bounds.size.height = bounds.size.width / ratio
+            } else {
+                bounds.size.height = maxSize
+                bounds.size.width = bounds.size.height * ratio
+            }
+        }
+        
+        let scaleRatio = bounds.size.width / width
+        let imageSize = CGSize(width: width, height: height)
+        var boundHeight : CGFloat = 0.0
+        
+        let ori = self.imageOrientation
+        
+        switch(ori) {
+        case .up:
+            transform = .identity
+            break
+            
+        case .down:
+            transform = CGAffineTransform(translationX: imageSize.width, y: imageSize.height)
+            transform = transform.rotated(by: CGFloat(Double.pi))
+            break
+            
+        case .left:
+            boundHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = boundHeight
+            transform = CGAffineTransform(translationX: 0.0, y: imageSize.width)
+            transform = transform.rotated(by: CGFloat(3.0 * Double.pi / 2.0))
+            break
+            
+        case .right:
+            boundHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = boundHeight
+            transform = CGAffineTransform(translationX: imageSize.height, y: 0.0)
+            transform = transform.rotated(by: CGFloat(Double.pi / 2.0))
+            break
+            
+        case .upMirrored:
+            transform = CGAffineTransform(translationX: imageSize.width, y: 0.0)
+            transform = transform.scaledBy(x: -1.0, y: -1.0)
+            break
+            
+        case .downMirrored:
+            transform = CGAffineTransform(translationX: 0.0, y: imageSize.height)
+            transform = transform.scaledBy(x: 1.0, y: -1.0)
+            break
+            
+        case .leftMirrored:
+            boundHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = boundHeight
+            transform = CGAffineTransform(translationX: imageSize.height, y: imageSize.width)
+            transform = transform.scaledBy(x: -1.0, y: 1.0)
+            transform = transform.rotated(by: CGFloat(3.0 * Double.pi / 2.0))
+            break
+            
+        case .rightMirrored:
+            boundHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = boundHeight
+            transform = CGAffineTransform(scaleX: -1.0, y: 1.0)
+            transform = transform.rotated(by: CGFloat(Double.pi / 2.0))
+            break
+        }
+        
+        UIGraphicsBeginImageContext(bounds.size)
+        
+        guard let context = UIGraphicsGetCurrentContext()
+            else { return self }
+        
+        if ori == UIImageOrientation.right || ori == UIImageOrientation.left {
+            context.scaleBy(x: -scaleRatio, y: scaleRatio)
+            context.translateBy(x: -height, y: 0.0)
+        } else {
+            context.scaleBy(x: scaleRatio, y: -scaleRatio)
+            context.translateBy(x: 0.0, y: -height)
+        }
+        
+        context.concatenate(transform)
+        context.draw(imgRef, in: CGRect(x: 0, y: 0, width: width, height: height))
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image ?? self
+    }
+}
+
+extension UITextField {
+    
+    @IBInspectable var maxLength: Int {
+        get {
+            // 4
+            guard let length = maxLengths[self] else {
+                return Int.max
+            }
+            return length
+        }
+        set {
+            maxLengths[self] = newValue
+            // 5
+            addTarget(
+                self,
+                action: #selector(limitLength),
+                for: UIControlEvents.editingChanged
+            )
+        }
+    }
+    
+    @objc func limitLength(textField: UITextField) {
+        // 6
+        guard let prospectiveText = textField.text,
+            prospectiveText.characters.count > maxLength
+            else {
+                return
+        }
+        
+        let selection = selectedTextRange
+        // 7
+        let maxCharIndex = prospectiveText.index(prospectiveText.startIndex, offsetBy: maxLength)
+        text = prospectiveText.substring(to: maxCharIndex)
+        selectedTextRange = selection
+    }
+    
+}
